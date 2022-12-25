cc6c4758aa0a982fdf4d29ab4cb2c626100026c0
Vukašin Igić <vukasinigic@Vukasins-Mac-Studio.local>
2022-12-25 23:11:26 +0100

    mongo

--- apps/states-of-matter/backend/dist/index.js
+++ apps/states-of-matter/backend/dist/index.js
@@ -10,7 +10,7 @@ import { cards } from '@som/shared/data';
 
 const settings = {
     mongo: {
-        uri: "mongodb://localhost:27017"
+        uri: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1"
     },
     eos: {
         endpoint: "https://testnet.telos.net",
